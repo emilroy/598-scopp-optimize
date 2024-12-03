271b7e8b92153e7df740cbab465e6100d1b4f831
Emil Roy <emiljroy@gmail.com>
2024-12-02 21:57:06 -0700

    added field based controller

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import monitoring_algorithms
 import environments as envs
 
 # Initialize environment class
-environment = envs.Debugger()
+environment = envs.MediumLafayetteFLood()
 
 # Initialize monitoring algorithm instance
 way_point_allocator = monitoring_algorithms.QLB(environment, number_of_robots=30,plot="full")
